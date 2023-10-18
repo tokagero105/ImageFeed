0abe7d323b9c896617d19c0d9f665567a32d2dd1
Semyon Titov <tokagero105@yandex.ru>
2023-10-18 23:43:43 +0300

    realize scrollView

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,57 @@
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            SingleImageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+    }
+    
+    @IBOutlet var SingleImageView: UIImageView!
+    @IBOutlet var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        SingleImageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        SingleImageView
+    }
+}
+
+extension SingleImageViewController {
+    
+    func rescaleAndCenterImageInScrollView(image: UIImage) {
+        
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScle = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScle, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
