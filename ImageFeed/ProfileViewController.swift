984d8421575b051a33feadd685b3ff8d4ccb19a3
Semyon Titov <tokagero105@yandex.ru>
2023-11-09 01:36:56 +0300

    Migrated implementation of ProfileViewController from Storyboard to Code

--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -2,15 +2,51 @@ import Foundation
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    
-    @IBOutlet var userAvatarImage: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var nickNameLabel: UILabel!
-    @IBOutlet var DescriptionLabel: UILabel!
-    
-    
-    @IBOutlet var logoutButton: UIButton!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadProfile()
+    }
+}
 
-    @IBAction func didTapLogoutButton() {
+extension ProfileViewController {
+    
+    func loadProfile() {
+        let profileImage = UIImage(named: "UserPick")
+        let profileImageView = UIImageView()
+        profileImageView.image = profileImage
+        view.addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant:20).isActive = true
+        profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+
+        let name = UILabel()
+        name.text = "Екатерина Новикова"
+        name.font = UIFont.systemFont(ofSize: 23.0, weight: .bold)
+        name.textColor = UIColor(named: "YP White")
+        view.addSubview(name)
+        name.translatesAutoresizingMaskIntoConstraints = false
+        name.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 20).isActive = true
+        name.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor).isActive = true
+        
+        let nickname = UILabel()
+        nickname.text = "@ekaterina_nov"
+        nickname.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        nickname.textColor = UIColor(named: "YP Gray")
+        view.addSubview(nickname)
+        nickname.translatesAutoresizingMaskIntoConstraints = false
+        nickname.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 8).isActive = true
+        nickname.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor).isActive = true
+        
+        let description = UILabel()
+        description.text = "Hello, world!"
+        description.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        description.textColor = UIColor(named: "YP White")
+        view.addSubview(description)
+        description.translatesAutoresizingMaskIntoConstraints = false
+        description.topAnchor.constraint(equalTo: nickname.bottomAnchor, constant: 8).isActive = true
+        description.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor).isActive = true
     }
 }
